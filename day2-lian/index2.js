971bd292da29a29d155dd5c69555129375a5cb08
lili <1979761695@qq.com>
2019-12-01 21:11:46 +0800

    最新作业

--- day2-lian/index2.js
+++ day2-lian/index2.js
@@ -1,19 +1,26 @@
-let http =require('http')
-let port=8080
+let http = require('http')
+let port = 8080
 
 const program = require('commander')
 
-program 
-    .version('1.1.0','-v --version')
+program
+    .version('1.1.0', '-v --version')
     .option('-p, --duan')
     .parse(process.argv)
 
-port=process.argv[3]
-if(program.duan)console.log(port)
+if (program.duan) {
 
-const server=http.createServer((req,res)=>{
+    if (process.argv[3] === undefined) {
+        port = 8080
+    } else {
+        port = process.argv[3]
+    }
+    console.log(port)
+}
+
+const server = http.createServer((req, res) => {
     res.end('111')
 })
-server.listen(port,()=>{
-    console.log("成功，端口9000")
+server.listen(port, () => {
+    console.log(`成功，端口${port}`)
 })
